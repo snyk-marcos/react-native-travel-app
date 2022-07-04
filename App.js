987708331cb89750874cfaef52346f4928a70198
Nana Kofi Larbi Mantey <nklmantey@st.ug.edu.gh>
2022-07-04 12:33:07 +0000

    welcome screen

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import TabNav from './src/navigation/TabNav'
 import { NavigationContainer } from '@react-navigation/native'
+import StackNav from './src/navigation/StackNav'
 
 const App = () => {
   return (
     <NavigationContainer>
-      <TabNav />
+      {/* <TabNav /> */}
+      <StackNav />
     </NavigationContainer>
   )
 }
